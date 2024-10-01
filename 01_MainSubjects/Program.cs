a6928d91275bd4725e3eac040836bc082281a8e1
bhdrd <bhdrd@DZPUZM>
2024-10-01 22:57:51 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; // Sistemin koddlama için kullandığı kütüphaneleri gösterir.
+
+namespace _01_MainSubjects // Projemizin adı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region YazdırmaKomutları
+            //Console.Write("Merhaba Dünya"); // Kodu bu şekilde yazıp çalıştırısan ekran görünmez bu sadece yazdırma kodudur.
+            //Console.WriteLine("Selam");
+
+            //    Console.WriteLine("***** Yemek Kategorileri *****");
+            //    Console.WriteLine();
+            //    Console.WriteLine("1-Çorbalar");
+            //    Console.WriteLine("2-Ana Yemekler");
+            //    Console.WriteLine("3-Soğuk Başlangıçlar");
+            //    Console.WriteLine("4-Salatalar");
+            //    Console.WriteLine("5-Tatlılar");
+            //    Console.WriteLine("6-İçecekler");
+            //    Console.WriteLine();
+            //    Console.WriteLine("***** Yemek Kategorileri *****");
+
+            #endregion
+
+            #region String Değişkenler
+
+            //String
+            //Değişken_türü değişken_adı;
+
+            //string name;
+            //name = "Bahadır";
+            //Console.Write(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Bahadır";
+            //customerSurname = "Değirmenci";
+            //customerPhone = "+90 553 488 21 86";
+            //customerEmail = "bhdrdegirmenci@gmail.com";
+            //district = "İskenderun";
+            //city = "HATAY";
+
+            //Console.WriteLine("**** Rezervasyon Kartı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("----------------------------------");
+            //Console.WriteLine("Müşteri; " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim:" + customerPhone);
+            //Console.WriteLine("Email Adresi:" + customerEmail);
+            //Console.WriteLine("Adres:" + district + "/" + city);
+            //Console.WriteLine("----------------------------------");
+
+            //customerName = "Merve";
+            //customerSurname = "Değirmenci";
+            //customerPhone = "+90 507 764 94 88";
+            //customerEmail = "test@gmail.com";
+            //district = "İskenderun";
+            //city = "HATAY";
+
+            //Console.WriteLine("----------------------------------");
+            //Console.WriteLine("Müşteri; " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim:" + customerPhone);
+            //Console.WriteLine("Email Adresi:" + customerEmail);
+            //Console.WriteLine("Adres:" + district + "/" + city);
+            //Console.WriteLine("----------------------------------");
+
+
+
+            #endregion
+
+            #region Int Değişşkenler
+
+            //int
+            //int number = 24;
+            //Console.WriteLine(number);
+
+            //int hamburgerPrice = 300;
+            //int cokePrice = 35;
+            //int waterPrice = 10;
+            //int friesPrice = 50;
+            //int pizzaPrice = 250;
+            //int lemonadePrice = 30;
+
+            //Console.WriteLine("**** Restoran Menü Fiyatı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("-----Hamburger Fiyatı:" + hamburgerPrice + " TL");
+            //Console.WriteLine("-----Pizza:" + pizzaPrice+ " TL");
+            //Console.WriteLine("-----Kola:"+ cokePrice + " TL");
+            //Console.WriteLine("-----Limonata:" + lemonadePrice +" TL");
+            //Console.WriteLine("-----Kızartma:" + friesPrice +" TL");
+            //Console.WriteLine("-----Su:" + waterPrice +" TL");
+            //Console.WriteLine();
+            //Console.WriteLine("**** Restoran Menü Fiyatı ****");
+
+            //Console.WriteLine();
+            //int hamburgerCount;
+            //int cokeCount;
+            //int waterCount;
+            //int friesCount;
+            //int pizzaCount;
+            //int lemonadeCount;
+
+            //int totalHamburgerPrice = 0;
+            //int totalCokePrice= 0;
+            //int totalWaterPrice = 0;
+            //int totalFriesPrice = 0;
+            //int totalPizzaPrice = 0;
+            //int totalLemonadePrice= 0;
+           
+
+            //hamburgerCount = 3;
+            //cokeCount = 3;
+            //waterCount = 3;
+            //friesCount = 1;
+            //pizzaCount = 0;
+            //lemonadeCount = 0;
+
+            //totalHamburgerPrice = hamburgerCount * hamburgerPrice;
+            //totalCokePrice = cokeCount * cokePrice;
+            //totalWaterPrice = waterCount * waterPrice;
+            //totalFriesPrice= friesCount * friesPrice;
+            //totalPizzaPrice= pizzaCount * pizzaPrice;
+            //totalLemonadePrice= lemonadeCount * lemonadePrice;
+
+
+            //Console.WriteLine("---------------------------------");
+            //Console.WriteLine("Hamburger Tutarı:" + totalHamburgerPrice + "TL");
+            //Console.WriteLine("Pizza Tutarı:" + totalPizzaPrice + "TL");
+            //Console.WriteLine("Kola Tutarı:" + totalCokePrice + "TL");
+            //Console.WriteLine("Kızartma Tutarı:" + totalFriesPrice + "TL");
+            //Console.WriteLine("Su Tutarı:" + totalWaterPrice + "TL");
+            //Console.WriteLine("Limonata Tutarı:" + totalLemonadePrice + "TL");
+
+            //Console.WriteLine();
+
+            //int totalPrice=totalCokePrice+totalFriesPrice+totalLemonadePrice+totalHamburgerPrice+totalPizzaPrice+totalWaterPrice;
+
+            //Console.WriteLine("Toplam Önedenecek Tutar:" +totalPrice);
+            
+
+
+
+            #endregion
+
+
+            Console.Read(); // Eğer ekrana yazdırmış olduğun kodu okumak istiyorsan yada ne olduğunu görmek istiyorsan Okuma kodu olan READ kullan.
+
+        }
+    }
+}
+
+
+
+
+//Yazdırma Komutları
